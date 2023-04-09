5e248422d10c9bfa4e7e7eb5cef3ee2b6ce1d39c
Zac <121649297+zac-march@users.noreply.github.com>
2023-04-09 14:52:19 +1000

    Implement recieve attack

--- src/Classes/Gameboard.ts
+++ src/Classes/Gameboard.ts
@@ -21,17 +21,42 @@ class Gameboard {
     this.ships = [];
   }
 
-  placeShip(ship: Ship, x: number, y: number, orientation: string) {
+  placeShip(shipObj: Ship, x: number, y: number, orientation: string) {
     const occupiedCells = [];
 
-    for (let i = 0; i < ship.length; i++) {
+    for (let i = 0; i < shipObj.length; i++) {
       occupiedCells.push([x, y]);
+      this.board[x][y] = 1;
       orientation === "vertical" ? (x += 1) : (y += 1);
     }
-    this.ships.push({ ship: ship, x: x, y: y, orientation: orientation });
+    this.ships.push({
+      shipObj,
+      occupiedCells,
+    });
   }
 
-  recieveAttack(x: number, y: number) {}
+  recieveAttack(x: number, y: number) {
+    let wasAttackSuccess = false;
+
+    this.ships.forEach((ship) => {
+      ship.occupiedCells.forEach((cell: number[]) => {
+        if (cell[0] === x && cell[1] === y) {
+          ship.shipObj.hit();
+          this.board[x][y] = -1;
+          wasAttackSuccess = true;
+          return;
+        }
+      });
+      if (wasAttackSuccess) return;
+    });
+
+    if (!wasAttackSuccess) this.board[x][y] = -2;
+  }
+
+  detectGameOver() {
+    const sunkValues = this.ships.map((ship) => ship.shipObj.isSunk());
+    return sunkValues.every((value) => value);
+  }
 }
 
 export { Gameboard };
--- src/Classes/tests/Gameboard.test.ts
+++ src/Classes/tests/Gameboard.test.ts
@@ -14,7 +14,7 @@ it("New gameboard is empty", () => {
   expect(getTotalValue(gameboard)).toBe(0);
 });
 
-it("Placed ship exists on gameboard", () => {
+it("Placed ship: ship exists on gameboard", () => {
   const gameboard = new Gameboard();
   gameboard.placeShip(new Ship(3), 2, 2, "vertical");
 
@@ -25,17 +25,39 @@ it("Placed ship exists on gameboard", () => {
   expect(gameboard.board[4][2]).toBe(1);
 });
 
-it("Place ship populates ship list", () => {
+it("Place ship: populates ship list", () => {
   const gameboard = new Gameboard();
   gameboard.placeShip(new Ship(5), 4, 3, "vertical");
   gameboard.placeShip(new Ship(2), 4, 3, "vertical");
 
   expect(gameboard.ships.length).toBe(2);
-  expect(gameboard.ships[0].ship.length).toEqual(5);
+  expect(gameboard.ships[0].shipObj.length).toEqual(5);
 });
 
-it("Place ship populates ship list", () => {
+it("Recieve attack: ship can be hit", () => {
   const gameboard = new Gameboard();
   gameboard.placeShip(new Ship(5), 4, 3, "vertical");
   gameboard.recieveAttack(4, 3);
+
+  expect(gameboard.board[4][3]).toBe(-1);
+});
+
+it("Recieve attack: attack can miss", () => {
+  const gameboard = new Gameboard();
+  gameboard.recieveAttack(4, 3);
+
+  expect(gameboard.board[4][3]).toBe(-2);
+});
+
+it("Can detect game over", () => {
+  const gameboard = new Gameboard();
+  gameboard.placeShip(new Ship(3), 4, 3, "vertical");
+  gameboard.placeShip(new Ship(2), 1, 1, "horizontal");
+  gameboard.recieveAttack(4, 3);
+  gameboard.recieveAttack(5, 3);
+  gameboard.recieveAttack(6, 3);
+  gameboard.recieveAttack(1, 1);
+  gameboard.recieveAttack(1, 2);
+
+  expect(gameboard.detectGameOver()).toBe(true);
 });
