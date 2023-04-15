81e9f511725a41fbebffa15cdd08842ef0618c3e
Zac <121649297+zac-march@users.noreply.github.com>
2023-04-15 10:55:01 +1000

    Randomize next attack

--- src/Classes/PlayerAI.ts
+++ src/Classes/PlayerAI.ts
@@ -9,27 +9,40 @@ class PlayerAI extends Player {
   }
 
   takeTurn(rival: Player) {
-    let attackResult: string;
-    let attackedCell: number[];
+    let targetCells: number[][];
+    let usedAttackQueue: boolean = false;
     this.attackQueue.length;
     if (this.attackQueue.length <= 0) {
-      const attackableCells = rival.board.getAttackableCells();
-      const randCellIndex = Math.floor(Math.random() * attackableCells.length);
-      attackedCell = attackableCells[randCellIndex];
+      targetCells = rival.board.getAttackableCells();
     } else {
-      attackedCell = this.attackQueue.shift();
+      targetCells = this.attackQueue;
+      usedAttackQueue = true;
     }
-    attackResult = rival.board.recieveAttack(attackedCell);
+
+    const randCellIndex = Math.floor(Math.random() * targetCells.length);
+    const attackedCell = targetCells[randCellIndex];
+    const attackResult = rival.board.recieveAttack(attackedCell);
+
+    if (usedAttackQueue) this.attackQueue.splice(randCellIndex, 1);
+
+    this.updateAttackQueue(attackResult, rival, attackedCell);
+  }
+
+  private updateAttackQueue(
+    attackResult: string,
+    rival: Player,
+    attackedCell: number[]
+  ) {
     if (attackResult === "hit") {
       if (rival.board.isShipSunk(attackedCell)) {
         this.attackQueue = [];
       } else {
-        this.updateAttackQueue(rival, attackedCell);
+        this.setAttackQueue(rival, attackedCell);
       }
     }
   }
 
-  updateAttackQueue(rival: Player, cell: number[]) {
+  setAttackQueue(rival: Player, cell: number[]) {
     let surroundingCells = [
       [cell[0] - 1, cell[1]],
       [cell[0] + 1, cell[1]],
