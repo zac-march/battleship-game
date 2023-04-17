e6945c72c9a5489b47a69dfeddda99e9a2ef9782
Zac <121649297+zac-march@users.noreply.github.com>
2023-04-17 16:25:25 +1000

    Add element to show game state

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import "./style.css";
 import { game } from "./Components/game";
 
+const main = document.querySelector("main");
+const container = document.createElement("div");
+main.append(container);
+
+const gameStateMessage = document.createElement("p");
+gameStateMessage.id = "game-message";
+container.append(gameStateMessage);
+
 game();
